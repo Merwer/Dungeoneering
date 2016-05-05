dad2f66755df6561eb57f0705aaafb0c5ea27bd0
Bill Belanger <wj.belanger@gmail.com>
2016-05-05 06:24:26 -0400

    Added modals to the index page that are triggered by the buttons. Right now these modals do nothing.

---
+++ Dungeoneering/Content/js/draftIndex.js
@@ -0,0 +1,25 @@
+﻿/*global jQuery, bootbox*/
+var chronicle = chronicle || {};
+chronicle.drafting = chronicle.drafting || {};
+chronicle.drafting.index = (function ($) {
+    "use strict";
+
+    var confirmDelete = function () {
+        bootbox.confirm("Are you sure you want to delete this deck?", function (response) {
+            if (response) {
+                // Trigger delete
+            }
+        });
+    };
+
+    var addMatch = function () {
+        bootbox.
+    };
+
+    var init = function () {
+        $('[data-modal="confirm-delete"]').click(confirmDelete);
+        $('[data-modal="add-match"]').click(addMatch);
+    };
+
+    init();
+}(jQuery));
