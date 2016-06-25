286216df6f49e7ac0826354f8a8d48c98befbcdf
Bill <wjblngr@gmail.com>
2016-06-25 06:34:01 -0400

    Added placeholder pages for My stats and global stats

---
+++ Dungeoneering/Controllers/StatsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Merwer.Chronicle.Dungeoneering.Tracker.Controllers
+{
+    public class StatsController : Controller
+    {
+        // GET: Stats
+        public ActionResult Global()
+        {
+            return View();
+        }
+        // GET: Stats
+        public ActionResult My()
+        {
+            return View();
+        }
+    }
+}
