7c106ee9bfc0e6ee575740501b08822d03c7981f
Bill <wjblngr@gmail.com>
2016-06-29 21:23:43 -0400

    Changed the requirement from email to username.

--- Dungeoneering/Models/AccountViewModels.cs
+++ Dungeoneering/Models/AccountViewModels.cs
@@ -6,8 +6,8 @@ namespace Merwer.Chronicle.Dungeoneering.Tracker.Models
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
     }
 
     public class ExternalLoginListViewModel
@@ -49,9 +49,8 @@ namespace Merwer.Chronicle.Dungeoneering.Tracker.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -65,9 +64,8 @@ namespace Merwer.Chronicle.Dungeoneering.Tracker.Models
     public class RegisterViewModel
     {
         [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
