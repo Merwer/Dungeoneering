ae4545441621dce03ed7fd44a5cd2adfc86746b1
Bill <wjblngr@gmail.com>
2016-06-29 20:47:31 -0400

    Initial stats

--- Dungeoneering/Controllers/StatsController.cs
+++ Dungeoneering/Controllers/StatsController.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Merwer.Chronicle.Dungeoneering.Tracker.Models;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace Merwer.Chronicle.Dungeoneering.Tracker.Controllers
 {
     public class StatsController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Stats
         public ActionResult Global()
         {
@@ -16,7 +19,55 @@ namespace Merwer.Chronicle.Dungeoneering.Tracker.Controllers
         // GET: Stats
         public ActionResult My()
         {
-            return View();
+            var drafts = db.Drafts.Include(d => d.Matches).Where(d => d.OwnerName == User.Identity.Name).ToList();
+            var games = drafts.SelectMany(d => d.Matches);
+
+            var data = CalculateStats<MyStatsData>(drafts);
+            if (drafts.Any())
+            {
+                data.TotalRewards = new MatchRewardList
+                {
+                    Copper = games.Sum(g => g.Rewards.Copper),
+                    Packs = games.Sum(g => g.Rewards.Packs),
+                    Shards = games.Sum(g => g.Rewards.Shards)
+                };
+                data.AverageRewards = new MatchRewardList
+                {
+                    Copper = games.Sum(g => g.Rewards.Copper) / drafts.Count(),
+                    Packs = games.Sum(g => g.Rewards.Packs) / drafts.Count(),
+                    Shards = games.Sum(g => g.Rewards.Shards) / drafts.Count()
+                };
+                data.ArianeStats = CalculateStats<LegendSpecificStatsData>(drafts.Where(d => d.Archetype == Archetype.Ariane).ToList());
+                data.OzanStats = CalculateStats<LegendSpecificStatsData>(drafts.Where(d => d.Archetype == Archetype.Ozan).ToList());
+                data.LinzaStats = CalculateStats<LegendSpecificStatsData>(drafts.Where(d => d.Archetype == Archetype.Linza).ToList());
+                data.RaptorStats = CalculateStats<LegendSpecificStatsData>(drafts.Where(d => d.Archetype == Archetype.TheRaptor).ToList());
+                data.VanesculaStats = CalculateStats<LegendSpecificStatsData>(drafts.Where(d => d.Archetype == Archetype.Vanescula).ToList());
+            }
+            return View(data);
+        }
+
+        private T CalculateStats<T>(List<Draft> drafts)
+            where T : BasicStatsData, new()
+        {
+            var games = drafts.SelectMany(d => d.Matches);
+            var data = new T();
+            if (!(drafts.Any() && games.Any()))
+            {
+                return data;
+            }
+            data.Drafts = drafts.Count();
+            data.Games = games.Count();
+            data.TotalWins = games.Count(g => g.Win);
+            data.TotalLosses = games.Count(g => !g.Win);
+            data.WinRate = data.TotalWins / ((double)data.Games);
+            data.AverageWins = drafts.Select(d => d.Matches.Count(g => g.Win)).Average();
+            data.FirstWinRate = games.Count(g => g.Win && g.First) / ((double)games.Count(g => g.First));
+            data.SecondWinRate = games.Count(g => g.Win && !g.First) / ((double)games.Count(g => !g.First));
+            data.ZeroToTwoWins = drafts.Count(d => d.Matches.Count(g => g.Win) >= 0 && d.Matches.Count(g => g.Win) <= 2);
+            data.ThreeToSixWins = drafts.Count(d => d.Matches.Count(g => g.Win) >= 3 && d.Matches.Count(g => g.Win) <= 6);
+            data.SevenToElevenWins = drafts.Count(d => d.Matches.Count(g => g.Win) >= 7 && d.Matches.Count(g => g.Win) <= 11);
+            data.TwelveWins = drafts.Count(d => d.Matches.Count(g => g.Win) >= 12);
+            return data;
         }
     }
 }
---
+++ Dungeoneering/Models/MyStatsData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Merwer.Chronicle.Dungeoneering.Tracker.Models
+{
+    /*
+     * Stats to be calculated:
+
+- General
+ - Total Drafts
+ - Total Games
+ - Win Rate
+ - Going First Win Rate
+ - Average Dungeon Depth
+ - Number of Times at each Depth
+ - Average Rewards
+ - Win Rate vs. Legend Y
+- Stats as Legend X
+ - Total Drafts
+ - Total Games
+ - Win Rate
+ - Going First Win Rate
+ - Average Dungeon Depth
+ - Number of Times at each Depth (0-2, 3-6, 7-11, 12)
+ - Win Rate vs. Legend Y
+- Total Rewards
+- Play Schedule (X times per day of the week)
+
+*/
+    public class BasicStatsData
+    {
+        public int Drafts { get; set; }
+        public int Games { get; set; }
+        public int TotalWins { get; set; }
+        public int TotalLosses { get; set; }
+        public double AverageWins { get; set; }
+        public double WinRate { get; set; }
+        public double FirstWinRate { get; set; }
+        public double SecondWinRate { get; set; }
+        public int ZeroToTwoWins { get; set; }
+        public int ThreeToSixWins { get; set; }
+        public int SevenToElevenWins { get; set; }
+        public int TwelveWins { get; set; }
+        // TODO: WinRate against Legend Y
+    }
+
+    public class LegendSpecificStatsData : BasicStatsData
+    {
+    }
+
+    public class MyStatsData : BasicStatsData
+    {
+        public MatchRewardList AverageRewards { get; set; }
+        public MatchRewardList TotalRewards { get; set; }
+
+        public LegendSpecificStatsData RaptorStats { get; set; }
+        public LegendSpecificStatsData ArianeStats { get; set; }
+        public LegendSpecificStatsData OzanStats { get; set; }
+        public LegendSpecificStatsData VanesculaStats { get; set; }
+        public LegendSpecificStatsData LinzaStats { get; set; }
+    }
+}
