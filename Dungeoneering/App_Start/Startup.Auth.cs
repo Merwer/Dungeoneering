051fa3b6c5da2dd6ca004f79c14d58565000f34c
Bill <wjblngr@gmail.com>
2016-07-30 14:08:00 -0400

    Added support for twitter authentication.

--- Dungeoneering/App_Start/Startup.Auth.cs
+++ Dungeoneering/App_Start/Startup.Auth.cs
@@ -7,6 +7,7 @@ using Microsoft.Owin.Security.Google;
 using Owin;
 using Merwer.Chronicle.Dungeoneering.Tracker.Models;
 using System.Configuration;
+using Microsoft.Owin.Security.Twitter;
 
 namespace Merwer.Chronicle.Dungeoneering.Tracker
 {
@@ -51,9 +52,22 @@ namespace Merwer.Chronicle.Dungeoneering.Tracker
             //    clientId: "",
             //    clientSecret: "");
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
+            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            {
+                ConsumerKey = ConfigurationManager.AppSettings["TwitterClientId"],
+                ConsumerSecret = ConfigurationManager.AppSettings["TwitterClientSecret"],
+                BackchannelCertificateValidator = new Microsoft.Owin.Security.CertificateSubjectKeyIdentifierValidator(new[]
+                {
+                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                    "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                    "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                    "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                    "‎add53f6680fe66e383cbac3e60922e3b4c412bed", // Symantec Class 3 EV SSL CA - G3
+                    "4eb6d578499b1ccf5f581ead56be3d9b6744a5e5", // VeriSign Class 3 Primary CA - G5
+                    "5168FF90AF0207753CCCD9656462A212B859723B", // DigiCert SHA2 High Assurance Server C‎A 
+                    "B13EC36903F8BF4701D498261A0802EF63642BC3" // DigiCert High Assurance EV Root CA
+                })
+            });
 
             //app.UseFacebookAuthentication(
             //   appId: "",
