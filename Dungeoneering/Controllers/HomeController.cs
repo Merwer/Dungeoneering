861aa2e1e35bd00ac319fd1dab247c0eb6d19787
Bill <wjblngr@gmail.com>
2016-08-06 21:06:40 -0400

    Removed About and contact pages.

--- Dungeoneering/Controllers/HomeController.cs
+++ Dungeoneering/Controllers/HomeController.cs
@@ -20,19 +20,5 @@ namespace Merwer.Chronicle.Dungeoneering.Tracker.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
